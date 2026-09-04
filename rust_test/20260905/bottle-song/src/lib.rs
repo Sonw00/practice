9b3f2ba77836fb4d78b0da45a099c2b0ff1cfb3b
Sonw00 <wyson@emstone.com>
2026-09-04 09:01:05 +0900

    test: update rust test difference of squares

--- rust_test/20260905/bottle-song/src/lib.rs
+++ rust_test/20260905/bottle-song/src/lib.rs
@@ -2,20 +2,31 @@ pub fn recite(start_bottles: u32, take_down: u32) -> String {
     let mut result = String::new();
 
     let numbers = [
-        "no","one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+        "No", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
     ];
 
-    for i in (1..=start_bottles).rev() {
+    for i in ((start_bottles - take_down + 1)..=start_bottles).rev() {
         let initial_bottle_count_word = numbers[(i) as usize];
-        let take_down_word = numbers[(take_down) as usize];
-        let remained_bottle_count_word = numbers[(i - take_down).max(0) as usize];
+        let initial_bottle_word = if i == 1 {
+            "bottle".to_string()
+        } else {
+            "bottles".to_string()
+        };
+        let remained_bottle_count_word = numbers[(i - 1) as usize].to_lowercase();
+        let remained_bottle_word = if (i - 1) == 1 {
+            "bottle".to_string()
+        } else {
+            "bottles".to_string()
+        };
 
         result.push_str(&format!(
-            "{} green bottles hanging on the wall,\n{} green bottles hanging on the wall,\nAnd if {} green bottle should accidentally fall,\nThere'll be {} green bottles hanging on the wall.",
+            "{} green {} hanging on the wall,\n{} green {} hanging on the wall,\nAnd if one green bottle should accidentally fall,\nThere'll be {} green {} hanging on the wall.",
             initial_bottle_count_word,
+            initial_bottle_word,
             initial_bottle_count_word,
-            take_down_word,
+            initial_bottle_word,
             remained_bottle_count_word,
+            remained_bottle_word,
         ));
 
         if i > 1 {
